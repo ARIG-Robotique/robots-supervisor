9d62fb80891081f2aaaa23d71f6f4a9797a29ad0
Duy Le <ldduy1006@gmail.com>
2019-05-10 11:12:22 +0200

    Fix le point de départ

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -6,14 +6,14 @@ export const Constants = {
       height: 143
     },
     configInitJaune: {
-      x: 200,
-      y: 1780,
+      x: 250,
+      y: 1500,
       angle: 0,
       matchTime: 100000
     },
     configInitViolet: {
-      x: 2800,
-      y: 1780,
+      x: 2750,
+      y: 1500   ,
       angle: 180,
       matchTime: 100000
     }
