dbe4d8c4a978c3d71baae5b99e5599e53040568d
Damien SOREL <damien.sorel@sogelink.fr>
2020-06-20 13:55:17 +0200

    Mise en page

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,8 +1,25 @@
 import { Component } from '@angular/core';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
 
 @Component({
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
+
+  icons: [string, string][] = [];
+
+  constructor(private library: FaIconLibrary) {
+  }
+
+  loadIcons() {
+    const definitions = (this.library as any).definitions as { [prefix: string]: { [name: string]: IconDefinition } };
+
+    for (const [prefix, icons] of Object.entries(definitions)) {
+      for (const name of Object.keys(icons)) {
+        this.icons.push([prefix, name]);
+      }
+    }
+  }
 
 }
