07ae751632b46618eb979d5fd628a4cbcae3c4cf
Gregory DEPUILLE <gregory.depuille@gmail.com>
2024-04-28 14:34:59 +0200

    2024 Visu

--- src/app/components/misc/no-robot/no-robot.component.ts
+++ src/app/components/misc/no-robot/no-robot.component.ts
@@ -18,10 +18,8 @@ import { IconDefinition, IconName, IconPrefix } from '@fortawesome/fontawesome-s
             </div>
 
             <div class="row">
-                <div class="col-3"><img class="d-block m-auto" src="assets/3d/nerell.png" /></div>
-                <div class="col-3"><img class="d-block m-auto" src="assets/3d/odin.png" /></div>
-                <div class="col-3"><img class="d-block m-auto" src="assets/3d/elfa.png" /></div>
-                <div class="col-3"><img class="d-block m-auto" src="assets/3d/sauron.png" /></div>
+                <div class="col-3"><img class="d-block m-auto" src="assets/3d/2024_robot.png" /></div>
+                <div class="col-3"><img class="d-block m-auto" src="assets/3d/2024_pami.png" /></div>
             </div>
         </div>
 
